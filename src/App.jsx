bd4959ca79176ebcc88ef5e45dfe9bf0e08cc007
Landin Kasti <lkastiman@gmail.com>
2023-03-04 17:02:46 -0600

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import './App.css'
 import Flashcard from './components/Flashcard'
-import { useState } from 'react';
 
 function App() {
   const dictionary = [
-    {q: "With great power comes _____ ________?", a : "great responsibility"},
-    {q: "test1 q", a: "test1 a"},
-    {q: "test2 q", a: "test2 a"},
-    {q: "test3 q", a: "test3 a"},
+    {source: "(Spider-man)", q: "\"With great power comes _____ ________?\"", a : "\"great responsibility\""},
+    {source: "(Spider-man 2002 Movie)", q: "Does Spider-man have web-shooters?", a: "No!"},
+    {source: "(Spider-man 2002 Movie)", q: "What were Norman's last words to Peter?", a: "\"Don't tell Harry...\""},
+    {source: "(Spider-man 2 2004 Movie)", q: "Who says \"Intelligence is not a privilege, it's a gift. And you use it for the good of mankind.\"?", a: "Dr. Otto Octavius"},
+    {source: "(Spider-man 2002 Movie)", q: "Who says \"You're pathetically predictable. Like a moth to the flame\"?", a: "The Green Goblin"},
+    {source: "(Spider-man 1994 TV series)", q: "What technology cures Spider-man's mutation disease?", a: "The Neogenic Recombinator"},
+    {source: "(Spider-man 1994 TV series)", q: "Which student in Peter's class turns into a vampire?", a: "Michael Morbius"},
+    {source: "(Spider-man Comics)", q: "What did Spider-man bring back to earth from the first \"Secret Wars\"?", a: "The Black Suit"},
+    {source: "(Spider-man Comics)", q: "Who's the name behind the Scarlet Spider?", a: "Kaine Parker"},
+    {source: "(Spider-man Far From Home Movie)", q: "What does Aunt May call Peter's spider-sense?", a: "Peter Tingle"},
   ];
 
   const numCards = dictionary.length;
@@ -16,7 +21,7 @@ function App() {
     <div className="app">
         <h1>Do You Know Spider-man Trivia?</h1>
         <div className='description'>
-          <p>How good is your knowledge on all types of Spider-man trivia?<br/>Test your knowledge to find out if you're a real web-head fan!<br/>Lets get started True Believers!</p>
+          <p>Test your knowledge to find out if you're a real web-head fan!<br/>The title in the {"( )"} indicates the movie/media of the trivia.<br/>Lets get started True Believers!</p>
           <p>Total number of cards: {numCards}</p>
         </div>
         <Flashcard dictionary={dictionary}/>
--- src/components/Flashcard.jsx
+++ src/components/Flashcard.jsx
@@ -2,10 +2,11 @@ import react from "react"
 import { useState } from 'react'
 import "./flashcard.css"
 const Flashcard = (props) => {
-    const [showFront, setShowFront] = useState(true)
     const [question, setQuestion] = useState(props.dictionary[0].q)
     const [answer, setAnswer] = useState(props.dictionary[0].a)
-    
+    const [mediaSource, setMediaSource] = useState(props.dictionary[0].source)
+    const [isFlipped, setIsFlipped] = useState(false)
+
     const handleChangeCard = () => {
         //generate a random integer between 0 and numCards-1
         //if the current card matches the card at that index then repeat
@@ -15,25 +16,32 @@ const Flashcard = (props) => {
         {
             index = Math.floor(Math.random() * props.dictionary.length);
         }
-
-        setQuestion(props.dictionary[index].q)
-        setAnswer(props.dictionary[index].a)
-        setShowFront(true)
+        setIsFlipped(false)
+        setTimeout(() => {
+            setQuestion(props.dictionary[index].q)
+            setAnswer(props.dictionary[index].a)
+            setMediaSource(props.dictionary[index].source)}
+            , 150)
     }
 
     const flipCard = () => {
-        setShowFront(!showFront)
+        setIsFlipped(!isFlipped)
     }
 
     return(
         <>
-            {/**render front by default
-             * if card clicked on, render back and stop rendering front. Or just invert
-             */}
-            <div onClick={flipCard} className="flashcard">
-                <p className="flashcard-info">{(showFront)? question : answer}</p>
+            <div className="scene">
+                <div className={`card ${isFlipped? "is-flipped" : ""}`}>
+                    <div className="flashcard front" onClick={flipCard}>
+                        <p className="flashcard-source">{mediaSource}</p>
+                        <p className="flashcard-info">{question}</p>
+                    </div>
+                    <div className="flashcard back" onClick={flipCard}>
+                        <p className="flashcard-info push-center">{answer}</p>
+                    </div>
+                </div>
             </div>
-            <button onClick={handleChangeCard}>new card</button>
+            <button onClick={handleChangeCard}>↺</button>
         </>
     );
 }
